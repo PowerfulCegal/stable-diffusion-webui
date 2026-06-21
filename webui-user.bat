de02d783a98e4ee024e861b95c24740aec097ae9
Alan <goffenhauser85@gmail.com>
2026-06-21 10:10:05 -0700

    Commit

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON=C:\Users\Alan\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\python.exe
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--api
 
 call webui.bat
